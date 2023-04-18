fec8cdcf974bd80176357ce5bd5fea9df43bf436
Anil Maharjan <simpleanilcha@gmail.com>
2023-04-19 00:18:15 +0545

    update: 1768. Merge Strings Alternately

--- 1768-Merge-Strings-Alternately.js
+++ 1768-Merge-Strings-Alternately.js
@@ -4,26 +4,13 @@
  * @return {string}
  */
 var mergeAlternately = function (word1, word2) {
-  let result = ''
-  const word1Arr = word1.split('')
-  const word2Arr = word2.split('')
-  let shortWord = ''
-  if (word1Arr.length < word2Arr.length) shortWord = 'one'
-  else if (word1Arr.length > word2Arr.length) shortWord = 'two'
-  else shortWord = ''
+  let merged = ''
+  const maxNum = Math.max(word1.length, word2.length)
 
-  for (let i = 0; i < word1Arr.length; i++) {
-    for (let j = 0; j < word2Arr.length; j++) {
-      if (i == j) {
-        result += word1Arr[i]
-        result += word2Arr[j]
-      }
-    }
+  for (let i = 0; i < maxNum; i++) {
+    if (word1[i]) merged += word1[i]
+    if (word2[i]) merged += word2[i]
   }
 
-  let tail = ''
-  if (shortWord === 'one') tail = word2Arr.slice(word1Arr.length).join('')
-  else if (shortWord === 'two') tail = word1Arr.slice(word2Arr.length).join('')
-
-  return result + tail
+  return merged
 };
