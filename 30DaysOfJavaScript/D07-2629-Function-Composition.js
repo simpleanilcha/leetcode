f4198fb7a34c7b3403cb339d01f8d235eda5d211
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-11 19:51:20 +0545

    Day 07 - 2629. Function Composition

---
+++ 30DaysOfJavaScript/D07-2629-Function-Composition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  // Approach 1: Function Composition using Iteration with for...loop
+  return function (x) {
+    if (!functions.length) return x;
+    let input = x
+    for (let i = functions.length - 1; i >= 0; i--) {
+      input = functions[i](input)
+    }
+    return input
+  }
+};
+
+/**
+* const fn = compose([x => x + 1, x => 2 * x])
+* fn(4) // 9
+*/
