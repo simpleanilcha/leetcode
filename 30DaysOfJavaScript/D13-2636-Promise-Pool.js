0a22aba81d2ecb61ae83107f040600e970cb4ea5
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-18 03:08:50 +0545

    Day 13 - 2636. Promise Pool

---
+++ 30DaysOfJavaScript/D13-2636-Promise-Pool.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+
+var promisePool = async function (functions, n) {
+  let currentIndex = 0
+  const next = async () => {
+    const fn = functions[currentIndex++]
+    if (fn) {
+      await fn();
+      return next()
+    }
+  }
+  await Promise.all(functions.slice(0, n).map(next))
+};
+
+/**
+* const sleep = (t) => new Promise(res => setTimeout(res, t));
+* promisePool([() => sleep(500), () => sleep(400)], 1)
+*   .then(console.log) // After 900ms
+*/
