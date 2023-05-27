eed59079bb4b346416b4ea4fd0ad658485c849a6
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-27 06:07:02 +0545

    Day 23 - 2619. Array Prototype Last

---
+++ 30DaysOfJavaScript/D23-2619-Array-Prototype-Last.js
@@ -0,0 +1,10 @@
+Array.prototype.last = function () {
+  // Array.prototype.at() Method
+  if (this.length <= 0) return -1;
+  return this.at(-1);
+};
+
+/**
+* const arr = [1, 2, 3];
+* arr.last(); // 3
+*/
