17f2fa33d57a3bc99a409455c8295de4772d5297
Anil Maharjan <simpleanilcha@gmail.com>
2023-10-02 23:26:38 +0545

    complete: 2038. Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+var winnerOfGame = function (colors) {
+  let a = 0;
+  let b = 0;
+
+  for (let i = 2; i < colors.length; i++) {
+    if (colors[i - 2] === 'A' && colors[i - 1] === 'A' && colors[i] === 'A') {
+      a++;
+    }
+    else if (colors[i - 2] === 'B' && colors[i - 1] === 'B' && colors[i] === 'B') {
+      b++;
+    }
+  }
+  return a > b;
+};
