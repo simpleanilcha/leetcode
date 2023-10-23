5c8757f565036d1bfea5c360ae042e58ebd56a52
Anil Maharjan <simpleanilcha@gmail.com>
2023-10-23 21:27:31 +0545

    complete: 342. Power of Four

---
+++ 342-Power-of-Four.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function (n) {
+  let res = false
+  let count = 1
+  for (let i = 0; i < 100; i++) {
+    if (n == count) {
+      res = true
+      break
+    }
+    count *= 4
+  }
+  return res
+};
