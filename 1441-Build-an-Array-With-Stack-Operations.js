c6d11f12bfb0dae422c1dd1a061c1240985e3b28
Anil Maharjan <simpleanilcha@gmail.com>
2023-11-03 22:53:29 +0545

    complete: 1441. Build an Array With Stack Operations

---
+++ 1441-Build-an-Array-With-Stack-Operations.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} target
+ * @param {number} n
+ * @return {string[]}
+ */
+var buildArray = function (target, n) {
+  let res = [];
+  let j = 0;
+  for (let i = 1; i <= n; i++) {
+    if (target[j] === i) {
+      res.push('Push');
+      j++;
+    } else res.push(...['Push', 'Pop']);
+    if (j === target.length) return res;
+  }
+  return res;
+};
