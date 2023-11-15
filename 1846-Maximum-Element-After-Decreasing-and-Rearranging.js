49e74813fe9b5f5afa193d4c993930a141d2a1d8
Anil Maharjan <simpleanilcha@gmail.com>
2023-11-15 20:30:39 +0545

    complete: 1846. Maximum Element After Decreasing and Rearranging

---
+++ 1846-Maximum-Element-After-Decreasing-and-Rearranging.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function (arr) {
+  arr.sort((a, b) => a - b);
+  let maxVal = 1;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxVal) {
+      maxVal += 1;
+    }
+  }
+
+  return maxVal;
+};
