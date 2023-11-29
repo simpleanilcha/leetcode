0cee14449be7d434cb6e7edf9e2d506acdb71134
Anil Maharjan <simpleanilcha@gmail.com>
2023-11-29 23:30:02 +0545

    complete: 191. Number of 1 Bits

---
+++ 191-Number-of-1-Bits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let count = 0;
+  let mask = 1;
+
+  for (let i = 0; i < 32; i++) {
+    if ((mask & n) !== 0) {
+      count++;
+    }
+    mask <<= 1;
+  }
+
+  return count;
+};
