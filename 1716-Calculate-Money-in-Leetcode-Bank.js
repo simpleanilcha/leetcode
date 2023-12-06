5592405a1e4b40048d52cb063c178685f8b6c4eb
Anil Maharjan <simpleanilcha@gmail.com>
2023-12-06 23:29:35 +0545

    complete: 1716. Calculate Money in Leetcode Bank

---
+++ 1716-Calculate-Money-in-Leetcode-Bank.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function totalMoney(n) {
+  let totalMoney = 0;
+  let dailyDeposit = 1;
+  let mondayDeposit = 1;
+
+  for (let day = 1; day <= n; day++) {
+    totalMoney += dailyDeposit;
+
+    if ((day % 7) !== 0) {
+      dailyDeposit++;
+    } else {
+      mondayDeposit++;
+      dailyDeposit = mondayDeposit;
+    }
+  }
+
+  return totalMoney;
+}
