b657e2404a96a5de5d478ae4790412061941ebd5
Anil Maharjan <simpleanilcha@gmail.com>
2024-01-20 22:51:04 +0545

    complete: 907. Sum of Subarray Minimums

---
+++ 907-Sum-of-Subarray-Minimums.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumSubarrayMins = function (arr) {
+  const MOD = 1000000007;
+  const stack = [];
+  let sumOfMinimums = 0;
+
+  for (let i = 0; i <= arr.length; i++) {
+    while (stack.length && (i === arr.length || arr[stack[stack.length - 1]] >= arr[i])) {
+      const mid = stack.pop();
+      const leftBoundary = stack.length === 0 ? -1 : stack[stack.length - 1];
+      const rightBoundary = i;
+
+      const count = (mid - leftBoundary) * (rightBoundary - mid) % MOD;
+
+      sumOfMinimums += (count * arr[mid]) % MOD;
+      sumOfMinimums %= MOD;
+    }
+    stack.push(i);
+  }
+
+  return sumOfMinimums;
+};
