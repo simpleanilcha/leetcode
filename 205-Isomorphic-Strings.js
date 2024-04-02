03c274064d8fda92c4c40f1ddc466933e7dcb27f
Anil Maharjan <simpleanilcha@gmail.com>
2024-04-02 23:51:45 +0545

    complete: 205. Isomorphic Strings

---
+++ 205-Isomorphic-Strings.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) !== t.indexOf(t[i])) return false
+  }
+  return true
+};
